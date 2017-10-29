76ee48ba71b695682d3259a7606073e91c33d6e6
Livia Galeazzi <liviagal@gmail.com>
2017-10-29 21:25:25 +0100

    Add Role DAO

---
+++ src/main/java/com/instateams/dao/RoleDao.java
@@ -0,0 +1,13 @@
+package com.instateams.dao;
+
+import com.instateams.model.Role;
+
+import java.util.List;
+
+public interface RoleDao
+{
+    List<Role> findAll();
+    Role findById(Long id);
+    void save(Role role);
+    void delete(Role role);
+}
