bca12d7c4c767992eaa7644208816fa99e690c6f
Livia Galeazzi <liviagal@gmail.com>
2017-10-29 20:49:15 +0100

    Add Hibernate configuration

--- src/main/java/com/instateams/App.java
+++ src/main/java/com/instateams/App.java
@@ -1,5 +1,15 @@
 package com.instateams;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@EnableAutoConfiguration
+@ComponentScan
 public class App
 {
+    public static void main(String[] args)
+    {
+        SpringApplication.run(App.class, args);
+    }
 }
