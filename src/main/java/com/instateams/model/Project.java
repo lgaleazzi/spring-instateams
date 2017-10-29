4be9e9a428ff2b68fd870dff7e404d6bb0578136
Livia Galeazzi <liviagal@gmail.com>
2017-10-29 21:11:36 +0100

    Add Project model

---
+++ src/main/java/com/instateams/model/Project.java
@@ -0,0 +1,94 @@
+package com.instateams.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Entity
+public class Project
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Column
+    private String name;
+
+    @NotNull
+    @Column
+    private String description;
+
+    @Column
+    private String status;
+
+    @ManyToMany
+    @Column
+    private List<Role> rolesNeeded;
+
+    @ManyToMany
+    @Column
+    private List<Collaborator> collaborators;
+
+    public Project() {}
+
+    public Long getId()
+    {
+        return id;
+    }
+
+    public void setId(Long id)
+    {
+        this.id = id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+
+    public String getStatus()
+    {
+        return status;
+    }
+
+    public void setStatus(String status)
+    {
+        this.status = status;
+    }
+
+    public List<Role> getRolesNeeded()
+    {
+        return rolesNeeded;
+    }
+
+    public void setRolesNeeded(List<Role> rolesNeeded)
+    {
+        this.rolesNeeded = rolesNeeded;
+    }
+
+    public List<Collaborator> getCollaborators()
+    {
+        return collaborators;
+    }
+
+    public void setCollaborators(List<Collaborator> collaborators)
+    {
+        this.collaborators = collaborators;
+    }
+}
