4caa24cd0d6bf7bc1e6314dbb1a8b24037a73280
Livia Galeazzi <liviagal@gmail.com>
2017-10-30 18:22:14 +0100

    Implement Add and display collaborators

---
+++ src/main/java/com/instateams/controller/CollaboratorController.java
@@ -0,0 +1,47 @@
+package com.instateams.controller;
+
+import com.instateams.model.Collaborator;
+import com.instateams.model.Role;
+import com.instateams.service.CollaboratorService;
+import com.instateams.service.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@Controller
+public class CollaboratorController
+{
+    @Autowired
+    private CollaboratorService collaboratorService;
+
+    @Autowired
+    private RoleService roleService;
+
+    @RequestMapping("/collaborators")
+    public String allCollaborators(Model model)
+    {
+        List<Collaborator> collaborators = collaboratorService.findAll();
+        List<Role> roles = roleService.findAll();
+        model.addAttribute("collaborators", collaborators);
+        model.addAttribute("roles", roles);
+        if (!model.containsAttribute("collaboratorToSave"))
+        {
+            model.addAttribute("collaboratorToSave", new Collaborator());
+        }
+
+        return "collaborator/index";
+    }
+
+    @RequestMapping(value = "/collaborators", method = RequestMethod.POST)
+    public String addCollaborator(@Valid Collaborator collaborator)
+    {
+        System.out.println(collaborator);
+        collaboratorService.save(collaborator);
+        return "redirect:/collaborators";
+    }
+}
