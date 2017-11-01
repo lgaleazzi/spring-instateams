11faa9cffc00931c39403e370194f9272f0a82f2
Livia Galeazzi <liviagal@gmail.com>
2017-11-01 12:10:01 +0100

    Change Thymeleaf version to Thymeleaf 3

--- src/main/java/com/instateams/App.java
+++ src/main/java/com/instateams/App.java
@@ -1,11 +1,9 @@
 package com.instateams;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableAutoConfiguration
-@ComponentScan
+@SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration.class})
 public class App
 {
     public static void main(String[] args)
