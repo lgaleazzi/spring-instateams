a34b3aa6a4a3d304667e4b3c3de190d6d5833a9c
Livia Galeazzi <liviagal@gmail.com>
2017-11-01 13:40:12 +0100

    Create "filldata" method to preload data for manual tests

--- src/main/java/com/instateams/dao/CollaboratorDao.java
+++ src/main/java/com/instateams/dao/CollaboratorDao.java
@@ -1,12 +1,15 @@
 package com.instateams.dao;
 
 import com.instateams.model.Collaborator;
+import com.instateams.model.Role;
 
 import java.util.List;
 
 public interface CollaboratorDao
 {
     List<Collaborator> findAll();
+
+    List<Collaborator> findByRole(Role role);
     Collaborator findById(Long id);
     void save(Collaborator collaborator);
     void delete(Collaborator collaborator);
--- src/main/java/com/instateams/dao/CollaboratorDaoImpl.java
+++ src/main/java/com/instateams/dao/CollaboratorDaoImpl.java
@@ -1,6 +1,7 @@
 package com.instateams.dao;
 
 import com.instateams.model.Collaborator;
+import com.instateams.model.Role;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.List;
 
 @Repository
@@ -24,6 +26,20 @@ public class CollaboratorDaoImpl implements CollaboratorDao
         CriteriaQuery<Collaborator> criteria = builder.createQuery(Collaborator.class);
         criteria.from(Collaborator.class);
         List<Collaborator> collaborators = session.createQuery(criteria).getResultList();
+        session.close();
+
+        return collaborators;
+    }
+
+    @Override
+    public List<Collaborator> findByRole(Role role)
+    {
+        Session session = sessionFactory.openSession();
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<Collaborator> query = builder.createQuery(Collaborator.class);
+        Root<Collaborator> root = query.from(Collaborator.class);
+        query.select(root).where(builder.equal(root.get("role"), role));
+        List<Collaborator> collaborators = session.createQuery(query).getResultList();
         session.close();
 
         return collaborators;
