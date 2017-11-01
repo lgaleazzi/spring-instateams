1c12dafcfb10f984c6c0f332d7b4b1067c10245e
Livia Galeazzi <liviagal@gmail.com>
2017-11-01 13:39:53 +0100

    Create "filldata" method to preload data for manual tests

--- src/main/java/com/instateams/controller/ProjectController.java
+++ src/main/java/com/instateams/controller/ProjectController.java
@@ -49,6 +49,13 @@ public class ProjectController
         return "project/index";
     }
 
+    @RequestMapping("/data")
+    public String fillWithData()
+    {
+        fillData();
+        return "redirect:/";
+    }
+
     @RequestMapping("/projects/add")
     public String addForm(Model model)
     {
@@ -86,5 +93,36 @@ public class ProjectController
         model.addAttribute("project", project);
 
         return "project/form";
+    }
+
+    private void fillData()
+    {
+        Role role1 = new Role();
+        role1.setName("Project Manager");
+        Role role2 = new Role();
+        role2.setName("Developer");
+        Role role3 = new Role();
+        role3.setName("QA");
+        roleService.save(role1);
+        roleService.save(role2);
+        roleService.save(role3);
+
+        Project project1 = new Project();
+        project1.setName("Implement Instateams");
+        project1.setDescription("A cool project");
+        project1.setStatus(Status.RUNNING);
+        project1.getRoles().add(role1);
+        project1.getRoles().add(role2);
+
+        projectService.save(project1);
+
+        Project project2 = new Project();
+        project2.setName("Implement giflib");
+        project2.setDescription("Another cool project");
+        project2.setStatus(Status.ARCHIVED);
+        project2.getRoles().add(role2);
+        project2.getRoles().add(role3);
+
+        projectService.save(project2);
     }
 }
