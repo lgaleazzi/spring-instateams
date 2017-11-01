1d79c2b7bdb6b95c8791e20b0fb5103801944152
Livia Galeazzi <liviagal@gmail.com>
2017-11-01 12:11:02 +0100

    Implement hashCode/equal/compareTo in Role

--- src/main/java/com/instateams/model/Role.java
+++ src/main/java/com/instateams/model/Role.java
@@ -4,7 +4,7 @@ import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
 @Entity
-public class Role
+public class Role implements Comparable<Role>
 {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -34,5 +34,44 @@ public class Role
     public void setName(String name)
     {
         this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+
+        Role role = (Role)o;
+
+        if (id != null ? !id.equals(role.id) : role.id != null)
+            return false;
+        return name != null ? name.equals(role.name) : role.name == null;
+
+    }
+
+    @Override
+    public int hashCode()
+    {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Role{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Role o)
+    {
+        return this.getName().compareTo(o.getName());
     }
 }
