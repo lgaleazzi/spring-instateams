0ef6771b8052e07a59a2bb315ceea1d0e95496c6
Livia Galeazzi <liviagal@gmail.com>
2017-11-05 20:46:16 +0100

    Refactor add abstract class GenericDao

--- src/main/java/com/instateams/dao/CollaboratorDaoImpl.java
+++ src/main/java/com/instateams/dao/CollaboratorDaoImpl.java
@@ -3,8 +3,6 @@ package com.instateams.dao;
 import com.instateams.model.Collaborator;
 import com.instateams.model.Role;
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.criteria.CriteriaBuilder;
@@ -13,28 +11,18 @@ import javax.persistence.criteria.Root;
 import java.util.List;
 
 @Repository
-public class CollaboratorDaoImpl implements CollaboratorDao
+public class CollaboratorDaoImpl extends GenericDao<Collaborator> implements CollaboratorDao
 {
-    @Autowired
-    private SessionFactory sessionFactory;
-
     @Override
-    public List<Collaborator> findAll()
+    protected Class<Collaborator> getPersistentClass()
     {
-        Session session = sessionFactory.openSession();
-        CriteriaBuilder builder = session.getCriteriaBuilder();
-        CriteriaQuery<Collaborator> criteria = builder.createQuery(Collaborator.class);
-        criteria.from(Collaborator.class);
-        List<Collaborator> collaborators = session.createQuery(criteria).getResultList();
-        session.close();
-
-        return collaborators;
+        return Collaborator.class;
     }
 
     @Override
     public List<Collaborator> findByRole(Role role)
     {
-        Session session = sessionFactory.openSession();
+        Session session = getSessionFactory().openSession();
         CriteriaBuilder builder = session.getCriteriaBuilder();
         CriteriaQuery<Collaborator> query = builder.createQuery(Collaborator.class);
         Root<Collaborator> root = query.from(Collaborator.class);
@@ -43,35 +31,5 @@ public class CollaboratorDaoImpl implements CollaboratorDao
         session.close();
 
         return collaborators;
-    }
-
-    @Override
-    public Collaborator findById(Long id)
-    {
-        Session session = sessionFactory.openSession();
-        Collaborator collaborator = session.get(Collaborator.class, id);
-        session.close();
-
-        return collaborator;
-    }
-
-    @Override
-    public void save(Collaborator collaborator)
-    {
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
-        session.saveOrUpdate(collaborator);
-        session.getTransaction().commit();
-        session.close();
-    }
-
-    @Override
-    public void delete(Collaborator collaborator)
-    {
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
-        session.delete(collaborator);
-        session.getTransaction().commit();
-        session.close();
     }
 }
